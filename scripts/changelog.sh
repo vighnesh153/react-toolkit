31bcf87d5bd72558457010302daa1f0a77419317
vighnesh153 <vighnesh.raut13@gmail.com>
2021-10-24 20:11:31 +0530

    chore: update changelog-stories.mdx

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -3,5 +3,5 @@
 # This script will copy the changelog from CHANGELOG.md to src/CHANGELOG.stories.mdx to be
 # picked up by storybook.
 
-echo "import { Meta } from \"@storybook/addon-docs/blocks\";\n\n<Meta title=\"Welcome/Changelog\" />\n\n" > ./src/Changelog.stories.mdx
-cat ./CHANGELOG.md >> ./src/Changelog.stories.mdx
+echo "import { Meta } from \"@storybook/addon-docs\";\n\n<Meta title=\"Welcome/Changelog\" />\n\n" > ./src/welcome/Changelog.stories.mdx
+cat ./CHANGELOG.md >> ./src/welcome/Changelog.stories.mdx
